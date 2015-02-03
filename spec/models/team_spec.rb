92189a4d3d5ad9cd92f0a58cea7b99d9d88d1ef5
Fernando <krajka.bartosz@gmail.com>
2015-02-03 18:19:48 +0100

    Team model specs added

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -13,4 +13,10 @@ RSpec.describe Team, :type => :model do
   it 'prints nicely' do
     expect(@team.to_s).to eq('Team-name (user@example.com)')
   end
+
+  it 'has default logo paths' do
+    expect(@team.logo_path).to eq('default_logo.jpg')
+    expect(@team.logo_thumb_path).to eq('default_logo_thumb.jpg')
+    expect(@team.logo_micro_path).to eq('default_logo_micro.jpg')
+  end
 end
