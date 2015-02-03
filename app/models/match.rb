c256796d2fa909440f7408cff74a2e8909ef9230
Fernando <krajka.bartosz@gmail.com>
2015-02-03 18:35:25 +0100

    Match model tests added. Some junk model tests removed.

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,8 @@ class Match < ActiveRecord::Base
   belongs_to :away, class_name: 'Team'
 
   has_many :match_events
+
+  def events
+    match_events.order(:time)
+  end
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Match, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has match events ordered by time inc' do
+    match = Match.new
+    event1 = MatchEvent.create!(:event_type => "goal", :time => 10, :match => match)
+    event3 = MatchEvent.create!(:event_type => "goal", :time => 30, :match => match)
+    event2 = MatchEvent.create!(:event_type => "yellow card", :time => 20, :match => match)
+
+    expect(match.events).to eq([event1, event2, event3])
+  end
 end
