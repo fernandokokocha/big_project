00907e98ba771a32a1dda159402da7abe21bd3d4
Fernando <krajka.bartosz@gmail.com>
2015-02-09 19:25:51 +0100

    Specs reworked

--- spec/routing/user-side_routing_spec.rb
+++ spec/routing/user-side_routing_spec.rb
@@ -1,37 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'User-side routing', :type => :routing do
-  describe 'routing' do
+  it 'routes to #my_team' do
+    expect(:get => '/my_team').to route_to('teams#my_team')
+  end
 
-    it 'routes to #index' do
-      expect(:get => '/teams').to route_to('teams#index')
-    end
+  it 'routes to #players' do
+    expect(:get => '/players').to route_to('teams#players')
+  end
 
-    it 'routes to #my_team' do
-      expect(:get => '/my_team').to route_to('teams#my_team')
-    end
+  it 'routes to #tactic' do
+    expect(:get => '/tactic').to route_to('teams#tactic')
+  end
+  it 'routes to #stadium' do
+    expect(:get => '/stadium').to route_to('teams#stadium')
+  end
+  it 'routes to #finance' do
+    expect(:get => '/finance').to route_to('teams#finance')
+  end
 
-    it 'routes to #players' do
-      expect(:get => '/players').to route_to('teams#players')
-    end
-
-    it 'routes to #tactic' do
-      expect(:get => '/tactic').to route_to('teams#tactic')
-    end
-    it 'routes to #stadium' do
-      expect(:get => '/stadium').to route_to('teams#stadium')
-    end
-    it 'routes to #finance' do
-      expect(:get => '/finance').to route_to('teams#finance')
-    end
-
-    it 'routes to #play_match' do
-      expect(:get => '/play_match').to route_to('matches#play_match')
-    end
-
-    it 'routes to admin panel' do
-      expect(:get => '/admin').to route_to('admin#index')
-    end
-
+  it 'routes to admin panel' do
+    expect(:get => '/admin').to route_to('admin#index')
   end
 end
